a6ea28ff56e2e2a8b20344dcd45d675ef7ae96ae
Vishal Chaware <30528484+vishalchaware@users.noreply.github.com>
2021-10-05 13:25:26 +0530

    User registration & password hashing

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -4,18 +4,15 @@ import User from '../models/userModel.js';
 
 const protect = asyncHandler(async (req, res, next) => {
 	let token;
-
+	// console.log(req.headers.authorization);
 	if (
 		req.headers.authorization &&
 		req.headers.authorization.startsWith('Bearer')
 	) {
 		try {
 			token = req.headers.authorization.split(' ')[1];
-
 			const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
 			req.user = await User.findById(decoded.id).select('-password');
-
 			next();
 		} catch (error) {
 			console.error(error);
@@ -23,7 +20,6 @@ const protect = asyncHandler(async (req, res, next) => {
 			throw new Error('Not authorized, token failed');
 		}
 	}
-
 	if (!token) {
 		res.status(401);
 		throw new Error('Not authorized, no token');
