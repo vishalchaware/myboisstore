860e9085a5c11e9451bb71661dc0e4b8c86486f0
Vishal Chaware <30528484+vishalchaware@users.noreply.github.com>
2021-10-17 14:19:06 +0530

    Top Product Carousel, Custom Page Title and Meta

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,7 +5,7 @@ import Product from '../models/productModel.js';
 // @route   GET /api/products
 // @access  Public
 const getProducts = asyncHandler(async (req, res) => {
-	const pageSize = 10;
+	const pageSize = 3;
 	const page = Number(req.query.pageNumber) || 1;
 	const keyword = req.query.keyword
 		? {
@@ -13,7 +13,7 @@ const getProducts = asyncHandler(async (req, res) => {
 					$regex: req.query.keyword,
 					$options: 'i',
 				},
-		}
+		  }
 		: {};
 	const count = await Product.countDocuments({ ...keyword });
 	const products = await Product.find({ ...keyword })
@@ -137,6 +137,15 @@ const createProductReview = asyncHandler(async (req, res) => {
 	}
 });
 
+// @desc    Get top rated products
+// @route   GET /api/products/top
+// @access  Public
+const getTopProducts = asyncHandler(async (req, res) => {
+	const products = await Product.find({}).sort({ rating: -1 }).limit(3);
+
+	res.json(products);
+});
+
 export {
 	getProducts,
 	getProductById,
@@ -144,4 +153,5 @@ export {
 	createProduct,
 	updateProduct,
 	createProductReview,
+	getTopProducts,
 };
