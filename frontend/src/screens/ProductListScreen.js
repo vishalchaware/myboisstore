17211d654fdcc247dc266cf4ad7816260586f571
Vishal Chaware <30528484+vishalchaware@users.noreply.github.com>
2021-10-17 11:44:55 +0530

    Update countInStock

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -95,6 +95,7 @@ const ProductListScreen = ({ history, match }) => {
 							<th>PRICE</th>
 							<th>CATEGORY</th>
 							<th>BRAND</th>
+							<th>STOCK</th>
 							<th></th>
 						</tr>
 					</thead>
@@ -106,6 +107,7 @@ const ProductListScreen = ({ history, match }) => {
 								<td>${product.price}</td>
 								<td>{product.category}</td>
 								<td>{product.brand}</td>
+								<td>{product.countInStock}</td>
 								<td>
 									<LinkContainer
 										to={`/admin/product/${product._id}/edit`}
