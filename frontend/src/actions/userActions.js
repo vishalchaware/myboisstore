5d5ddba7d6ccb9b5373cdcdcbfaa4613871dcea3
Vishal Chaware <30528484+vishalchaware@users.noreply.github.com>
2021-10-17 07:33:32 +0530

    Edit user screen and details

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -148,7 +148,6 @@ export const updateUserProfile = (user) => async (dispatch, getState) => {
 
 		const config = {
 			headers: {
-				'Content-Type': 'application/json',
 				Authorization: `Bearer ${userInfo.token}`,
 			},
 		};
@@ -219,7 +218,7 @@ export const deleteUser = (id) => async (dispatch, getState) => {
 			},
 		};
 
-		const { data } = await axios.delete(`/api/users/${id}`, config);
+		await axios.delete(`/api/users/${id}`, config);
 
 		dispatch({ type: USER_DELETE_SUCCESS });
 	} catch (error) {
